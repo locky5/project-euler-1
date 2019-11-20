f1310b4db325607f775e18b15ac90bfea13c7071
locky5 <daikevin19@yahoo.com>
2019-11-20 01:33:01 -0500

    solution to project euler question 1

---
+++ euler1.js
@@ -0,0 +1,12 @@
+function multiples(num) {
+  let sum = 0
+  for (let i=0; i<num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i
+    }
+  }
+
+  console.log(sum)
+}
+
+multiples(1000)
